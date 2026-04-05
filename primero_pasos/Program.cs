90b50a0d264a20d421886c1f92587dd3a2461784
elizz30 <eliasrpg1@gmail.com>
2026-04-04 21:03:36 -0600

    Jaabis (probando cosas nuevas)

--- primero_pasos/Program.cs
+++ primero_pasos/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Hola " + MyConst);
 
 */
 
-var MyDic = new Dictionary <string, int>
+var MyDic = new Dictionary<string, int>
 {
     {"Salchipapas", 1600},
     {"Nova Suprema", 3200},
@@ -16,5 +16,21 @@ var MyDic = new Dictionary <string, int>
 };
 
 Console.WriteLine("Que desea llevar?");
-Console.ReadLine();
-Console.WriteLine(MyDic["Salchipapas"]);
+
+dynamic respuesta = Console.ReadLine();
+if (respuesta == "Salchipapas")
+{
+    Console.WriteLine("El precio de las Salchipapas es: " + MyDic["Salchipapas"]);
+}
+else if (respuesta == "Nova Suprema")
+{
+    Console.WriteLine("El precio de la Nova Suprema es: " + MyDic["Nova Suprema"]);
+}
+else if (respuesta == "Taco Tico")
+{
+    Console.WriteLine("El precio del Taco Tico es: " + MyDic["Taco Tico"]);
+}
+else
+{
+    Console.WriteLine("No tenemos ese producto, gracias por su visita");
+}

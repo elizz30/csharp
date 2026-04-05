bf1247e7f63294a6ebcf200b21d8a1a169ba6934
elizz30 <eliasrpg1@gmail.com>
2026-04-04 20:49:27 -0600

    las primeras cosas en C#

---
+++ primero_pasos/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Console.WriteLine("Bienvenidos a mi primer programa en C# poco de hijueputas");
+
+const string MyConst = "Juan";
+
+
+Console.WriteLine("Hola " + MyConst);
+
+*/
+
+var MyDic = new Dictionary <string, int>
+{
+    {"Salchipapas", 1600},
+    {"Nova Suprema", 3200},
+    {"Taco Tico", 1500}
+};
+
+Console.WriteLine("Que desea llevar?");
+Console.ReadLine();
+Console.WriteLine(MyDic["Salchipapas"]);
